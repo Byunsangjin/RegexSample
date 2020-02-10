809cb0188a1ffca9026e3e5b17b98ced0e592719
Byunsangjin <qustkdwls1@naver.com>
2020-02-10 15:32:25 +0900

    TextField Delegate 구현

--- RegexSample/ViewController.swift
+++ RegexSample/ViewController.swift
@@ -12,9 +12,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let textFieldString:String = textField.text! + string
+        let nsString = textFieldString as NSString
+        let pattern:String = "^(8[0-6][0-3]\\d{2}|([1-7]\\d{4}|[1-9]\\d{3}|[1-9]\\d{2}|[1-9]\\d|\\d))(\\.([0-9]{1,2})?|\\.?)"
+        let range = NSMakeRange(0, textFieldString.count)
+
+        do {
+            let regex = try NSRegularExpression(pattern:pattern, options: [])
+            let matches = regex.matches(in:textFieldString, options:[], range:range)
+            
+            if matches.count == 0 {
+                return false;
+            }
+            
+            for match in matches {
+                let matchText = nsString.substring(with:match.range);
+                if matchText != textFieldString {
+                    return false
+                }
+            }
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        return true;
+    }
+}
