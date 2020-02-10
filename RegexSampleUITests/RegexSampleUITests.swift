adee4fa139b1a5b7b8b3ae6998d29dbee2e3c6d7
Byunsangjin <qustkdwls1@naver.com>
2020-02-10 16:17:20 +0900

    UITest

--- RegexSampleUITests/RegexSampleUITests.swift
+++ RegexSampleUITests/RegexSampleUITests.swift
@@ -27,17 +27,12 @@ class RegexSampleUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let inputTextField = app.textFields["InputTextField"]
+        inputTextField.tap()
+        inputTextField.typeText("86400.222")
+        
+        XCTAssertEqual(inputTextField.value as! String, "8640.22")
     }
-
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+    
+    
 }
